c9a1d2b1e0770d67c38d099c730900472695527f
Aria Diniz <aria.diniz.dev@gmail.com>
2024-07-16 16:52:17 -0300

    improving performance of dft

--- fourier_transform/dft.c
+++ fourier_transform/dft.c
@@ -6,22 +6,28 @@
 
 struct ArrayAndItem {
   double complex *array;
+  size_t index_start;
+  size_t index_end;
   size_t size;
   double complex *transform;
-  unsigned int index;
 };
 
 static void *calc_xn(void *arg) {
-  double complex *arr = ((struct ArrayAndItem *)arg)->array;
-  double complex *transform = ((struct ArrayAndItem *)arg)->transform;
-  unsigned int index = ((struct ArrayAndItem *)arg)->index;
-  size_t size = ((struct ArrayAndItem *)arg)->size;
-  double complex total = 0;
+  struct ArrayAndItem *temp = ((struct ArrayAndItem *)arg);
+  double complex *arr = temp->array;
+  double complex *transform = temp->transform;
+  size_t start = temp->index_start;
+  size_t end = temp->index_end;
+  size_t size = temp->size;
+  double complex total;
 
-  for (unsigned int j = 0; j < size; j++) {
-    total += arr[j] * cexp(-I * 2 * M_PI * index * j / size);
+  for (unsigned int k = start; k < end; k++) {
+    total = 0;
+    for (unsigned int j = 0; j < size; j++) {
+      total += arr[j] * cexp(-I * 2 * M_PI * k * j / size);
+    }
+    transform[k] = total;
   }
-  transform[index] = total;
   return NULL;
 }
 
@@ -37,27 +43,19 @@ int dft(double complex *data, size_t size, size_t n_threads) {
   
   for (unsigned int i = 0; i < n_threads; i++) {
     str[i].array = data;
-    str[i].size = size;
     str[i].transform = transform;
+    str[i].size = size;
   }
 
-  for (unsigned int i = 0; i < times; i++) {
-    for (unsigned int k = 0; k < n_threads; k++) {
-      str[k].index = k + i * n_threads;
-      pthread_create(&threads[k], NULL, calc_xn, &str[k]);
-    }
-    for (unsigned int k = 0; k < n_threads; k++) {
-      pthread_join(threads[k], NULL);
-    }
+  for (unsigned int k = 0; k < n_threads; k++) {
+    str[k].index_start = k * times;
+    str[k].index_end = (k + 1) * times;
+    if (k == n_threads - 1) str[k].index_end += remaining;
+    pthread_create(&threads[k], NULL, calc_xn, &str[k]);
   }
-
-  for (unsigned int i = 0; i < remaining; i++) {
-    str[i].index = i + times * n_threads;
-    pthread_create(&threads[i], NULL, calc_xn, &str[i]);
-  }
-
-  for (unsigned int i = 0; i < remaining; i++) {
-    pthread_join(threads[i], NULL);
+  
+  for (unsigned int k = 0; k < n_threads; k++) {
+    pthread_join(threads[k], NULL);
   }
 
   for (unsigned int i = 0; i < size; i++) {
