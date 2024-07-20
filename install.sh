d582f5cff2aa787de92ea85516cc536526280181
Aria Diniz <aria.diniz.dev@gmail.com>
2024-07-20 14:41:31 -0300

    improving shell install

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 # This script compiles and install the http_client library. 
 # Use it only if your Operating System is Linux and you have the gcc compiler installed.
 
+check_error () {
+  if [[ $? != 0 ]]; then
+    echo $1
+    exit 1
+  fi
+}
+
 if [[ "$1" == "" ]]; then
   echo "Usage: ./install.sh <library_name>"
   exit 1
@@ -13,18 +20,12 @@ readarray -t files < <(find "$directory" -type f \( -name "*.c" \))
 
 for file in "${files[@]}"; do
   gcc -c -fpic $file -o $file.o -I.
-  if [[ $? != 0  ]]; then
-    echo "Error compiling position independent code."
-    exit 1
-  fi
+  check_error "Error compiling position independent code."
   all_files+="$file.o "
 done
 
 gcc -shared -o lib$1.so $all_files
-if [[ $? != 0 ]]; then
-  echo "Error compiling shared object."
-  exit 1
-fi
+check_error "Error creating shared object."
 
 for file in "${files[@]}"; do
   rm -r $file.o
@@ -33,19 +34,11 @@ done
 echo "Compilation of $1.so finished."
 
 sudo cp lib$1.so /usr/lib
-
-if [[ $? != 0 ]]; then
-  echo "Error copying shared object to /usr/lib."
-  exit 1
-fi
+check_error "Error copying shared object to /usr/lib." 
 
 rm -r lib$1.so
 sudo cp $1.h /usr/include
-
-if [[ $? != 0 ]]; then
-  echo "Error copying header file to /usr/include."
-  exit 1
-fi
+check_error "Error copying header file to /usr/include."
 
 echo "Installation of $1.so finished."
 
