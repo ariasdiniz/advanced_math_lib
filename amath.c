67bd95a145b59cdc317990ac43fc80dbf63b6f0c
Aria Diniz <aria.diniz.dev@gmail.com>
2025-03-03 14:48:18 -0300

    creating amath cli tool (#1)
    - This PR includes an amath.c file and modifications to the Makefile and install.sh in order 
    to compile and install an executable to be called from command line via terminal. 
    Now, amath is a C library and also a CLI tool to transform data with mathematical functions.

---
+++ amath.c
@@ -0,0 +1,90 @@
+#include "amath.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <error.h>
+#include <string.h>
+
+#define HELP "AMath CLI\n"\
+             "Aria Diniz - 2025\n\n"\
+             "amath [CALC] - will calculate CALC for all values provided to STDIN\n\n"\
+             "[CALC] -> mean, median, stdev, ndist (Normal Distribution)\n"\
+             "This CLI does not handle complex numbers yet.\n"\
+
+#define TF "amath_temp_file.amath"
+
+#define BUFFER_SIZE 120
+
+static void store_data(size_t* count) {
+  char buffer[BUFFER_SIZE];
+  double temp_n;
+  FILE* temp = fopen(TF, "w");
+  while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+    *count += 1;
+    temp_n = atof(buffer);
+    fprintf(temp, "%lf\n", temp_n);
+  }
+  fclose(temp);
+}
+
+static double* allocate(size_t* count) {
+  double* data = malloc(sizeof(double) * *count);
+  if (data == NULL) {
+    fprintf(stderr, "Error allocating memory for data processing.");
+    return NULL;
+  }
+  return data;
+}
+
+static void read(FILE* temp, double* data, size_t* count) {
+  char buffer[BUFFER_SIZE];
+  for (size_t i = 0; i < *count; i++) {
+    if (fgets(buffer, BUFFER_SIZE, temp) == NULL) {
+      fprintf(stderr,  "Error parsing data.\n");
+    }
+    data[i] = atof(buffer);
+  }
+}
+
+static int transform(size_t* count, char* func) {
+  FILE* temp = fopen(TF, "r");
+  double* data = allocate(count);
+  if (data == NULL) return EXIT_FAILURE;
+
+  read(temp, data, count);
+  if (strcmp(func, "mean") == 0) {
+    printf("%lf\n", amath_mean(data, *count));
+  } else if (strcmp(func, "median") == 0) {
+    printf("%lf\n", amath_median(data, *count, 0));
+  } else if (strcmp(func, "stdev") == 0) {
+    printf("%lf\n", amath_stdev(data, *count));
+  } else if (strcmp(func, "ndist") == 0) {
+    double* dist = amath_ndist(data, *count, 4);
+    for (int i = 0; i < *count; i++) printf("%lf\n", dist[i]);
+    free(dist);
+  } else {
+    fprintf(stderr, "Unknown option: %s\n", func);
+    free(data);
+    fclose(temp);
+    remove(TF);
+    return EXIT_FAILURE;
+  }
+  free(data);
+  fclose(temp);
+  remove(TF);
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Amath needs at least one positional argument. Try --help\n");;
+    return EXIT_FAILURE;
+  }
+  if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+    printf(HELP);
+    return EXIT_SUCCESS;
+  }
+  size_t count = 0;
+  store_data(&count);
+  if (transform(&count, argv[1])) return EXIT_FAILURE;
+  return EXIT_SUCCESS;
+}
